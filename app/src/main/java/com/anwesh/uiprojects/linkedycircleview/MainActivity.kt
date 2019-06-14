e4f05382ae67596eaecd4126eecaf7ac618804a4
Anwesh43 <anweshthecool0@gmail.com>
2019-06-15 01:25:51 +0530

    adding YCircleView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedycircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedycircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedycircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.ycircleview.YCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        YCircleView.create(this)
     }
 }
