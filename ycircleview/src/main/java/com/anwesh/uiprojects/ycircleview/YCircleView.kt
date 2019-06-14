af11c5838b27bb5a3b8bd0539aac53d5f1af3396
Anwesh43 <anweshthecool0@gmail.com>
2019-06-15 00:58:24 +0530

    adding libraries and constants

--- ycircleview/src/main/java/com/anwesh/uiprojects/ycircleview/YCircleView.kt
+++ ycircleview/src/main/java/com/anwesh/uiprojects/ycircleview/YCircleView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.ycircleview
  * Created by anweshmishra on 15/06/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val circles : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
